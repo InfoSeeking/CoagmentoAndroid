da567811a566706f2981a02f87fad782bff12ba7
shahhyash <shahhyash@outlook.com>
2016-02-07 17:47:25 -0500

    Successfully implemented viewpager layout

--- src/main/java/org/coagmento/android/MainActivity.java
+++ src/main/java/org/coagmento/android/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,12 +18,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.florent37.materialviewpager.MaterialViewPager;
+import com.github.florent37.materialviewpager.header.HeaderDesign;
 import com.squareup.okhttp.ResponseBody;
 
 import org.coagmento.android.data.EndpointsInterface;
 import org.coagmento.android.fragment.MainFragment;
+import org.coagmento.android.fragment.RecyclerViewFragment;
 import org.coagmento.android.models.Errors;
 import org.coagmento.android.models.ProjectListResponse;
 import org.coagmento.android.models.Result;
@@ -44,10 +52,12 @@ public class MainActivity extends AppCompatActivity
     //UI References
     private TextView mHeaderNameView;
     private TextView mHeaderEmailView;
+    private TextView toolbarTitleView;
     private AlertDialog alertDialog;
     private ProgressDialog progressDialog;
 
     // Menu References
+    private MaterialViewPager mViewPager;
     private Toolbar toolbar;
     private MenuItem previousItem;
     private NavigationView navigationView;
@@ -160,8 +170,27 @@ public class MainActivity extends AppCompatActivity
     }
 
     protected void initializeUI() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        // Set tabs on toolbar
+        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+
+        toolbar = mViewPager.getToolbar();
+        toolbar.setTitle("");
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+
+            final ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+                actionBar.setDisplayShowTitleEnabled(true);
+                actionBar.setDisplayUseLogoEnabled(false);
+                actionBar.setHomeButtonEnabled(true);
+            }
+        }
 
         // Set User Name and Email in navigation header
         mHeaderNameView = (TextView) findViewById(R.id.navview_header_name);
@@ -181,9 +210,8 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
+        // Add Projects to Nav Menu
 
-//        // Add Projects to Nav Menu
-//
         myProjectList = sortProjects(projectList, false);
         sharedProjectList = sortProjects(projectList, true);
 
@@ -211,42 +239,128 @@ public class MainActivity extends AppCompatActivity
         MenuItem mi = menu.getItem(menu.size() - 1);
         mi.setTitle(mi.getTitle());
 
-        //Initialize the navigation view withe first project
-        if (currentProject!= null) {
-            previousItem = projectIdHashMap.get(currentProject.getProjectId());
-            if(previousItem!=null) {
-                currentProject = menuItemHashMap.get(previousItem);
-                previousItem.setChecked(true);
-                toolbar.setTitle(previousItem.getTitle());
-            } else {
+        toolbarTitleView = (TextView) findViewById(R.id.logo_white);
+
+        if(projectList.size() != 0) {
+            //Initialize the navigation view withe first project
+            if (currentProject != null) {
+                previousItem = projectIdHashMap.get(currentProject.getProjectId());
+                if (previousItem != null) {
+                    currentProject = menuItemHashMap.get(previousItem);
+                    previousItem.setChecked(true);
+                    toolbarTitleView.setText(previousItem.getTitle());
+                } else {
+                    loadFirstProject();
+                }
+            } else if (currentProject == null && (myProjectList.size() > 0 || sharedProjectList.size() > 0)) {
                 loadFirstProject();
             }
+        } else {
+            toolbarTitleView.setText(R.string.no_projects_found);
+
         }
-        else if(currentProject == null && (myProjectList.size() > 0 || sharedProjectList.size() > 0)) {
-            loadFirstProject();
-        }
 
-        //Load Main Fragment
+        // Set ViewPager
 
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
-        if (findViewById(R.id.fragment_container) != null) {
+        mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
+            @Override
+            public Fragment getItem(int position) {
+                switch (position % 4) {
+                    //case 0:
+                    //    return RecyclerViewFragment.newInstance();
+                    //case 1:
+                    //    return RecyclerViewFragment.newInstance();
+                    //case 2:
+                    //    return WebViewFragment.newInstance();
+                    default:
+                        return RecyclerViewFragment.newInstance();
+                }
+            }
 
-            // Create a new Fragment to be placed in the activity layout
-            MainFragment firstFragment = new MainFragment();
+            @Override
+            public int getCount() {
+                return 4;
+            }
 
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position % 4) {
+                    case 0:
+                        return "Bookmarks";
+                    case 1:
+                        return "Pages";
+                    case 2:
+                        return "Snippets";
+                    case 3:
+                        return "Documents";
+                }
+                return "";
+            }
+        });
 
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment).commit();
-        }
+        mViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
+            @Override
+            public HeaderDesign getHeaderDesign(int page) {
+                switch (page) {
+                    case 0:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.green,
+                                "http://www.theblogboutique.com/wp-content/uploads/2009/07/simply-a-fern-on-green-background-free-header-art.jpg");
+                    case 1:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.blue,
+                                "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
+                    case 2:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.cyan,
+                                "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+                    case 3:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.red,
+                                "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
+                }
+
+                //execute others actions if needed (ex : modify your header logo)
+
+                return null;
+            }
+        });
+
+        mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
+        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
+
+        View logo = findViewById(R.id.logo_white);
+        if (logo != null)
+            logo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mViewPager.notifyHeaderChanged();
+                    Toast.makeText(getApplicationContext(), "Yes, the title is clickable", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+//        //Load Main Fragment
+//
+//        // Check that the activity is using the layout version with
+//        // the fragment_container FrameLayout
+//        if (findViewById(R.id.fragment_container) != null) {
+//
+//            // However, if we're being restored from a previous state,
+//            // then we don't need to do anything and should return or else
+//            // we could end up with overlapping fragments.
+//
+//            // Create a new Fragment to be placed in the activity layout
+//            MainFragment firstFragment = new MainFragment();
+//
+//            // In case this activity was started with special instructions from an
+//            // Intent, pass the Intent's extras to the fragment as arguments
+//            firstFragment.setArguments(getIntent().getExtras());
+//
+//            // Add the fragment to the 'fragment_container' FrameLayout
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.fragment_container, firstFragment).commit();
+//        }
 
     }
 
--- src/main/java/org/coagmento/android/fragment/MainFragment.java
+++ src/main/java/org/coagmento/android/fragment/MainFragment.java
@@ -19,14 +19,6 @@ import org.coagmento.android.R;
  * create an instance of this fragment.
  */
 public class MainFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,20 +26,10 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
+    public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +38,6 @@ public class MainFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
